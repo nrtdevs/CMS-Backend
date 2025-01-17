629ccc488711ee0ebfb7201e788d3197d56af0b1
Dev <dev@newrise.in>
2025-01-17 15:10:40 +0530

    role import

--- app/routes/bidding.py
+++ app/routes/bidding.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from .token import verifyJWTToken
 from app.models.user import User, db
 from app.models.project import Project, db
-from app.models.role import Role
+
 
 biddings_bp = Blueprint('bidding_routes', __name__)
 
